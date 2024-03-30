88b71c13fdb0a81ffc21caa7516277c0066c6389
ricardocarvalho <ricardoddecarvalho@gmail.com>
2024-03-30 19:56:19 +0000

    feat: tabelas msg e notification criadas

--- demo/src/main/java/aor/paj/entity/NotificationEntity.java
+++ demo/src/main/java/aor/paj/entity/NotificationEntity.java
@@ -1,69 +1,69 @@
 package aor.paj.entity;//package aor.paj.entity;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.*;
-//
-//import java.time.LocalDateTime;
-//
-//@Entity
-//@Table (name = "notification")
-//public class NotificationEntity {
-//    private static final long serialVersionUID = 1L;
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "id", nullable = false, unique = true, updatable = false)
-//    private int id;
-//
-//    @ManyToOne
-//    @JoinColumn(name="user", nullable = false)
-//    private UserEntity user;
-//
-//    @OneToOne
-//    @JoinColumn(name="message", nullable = false)
-//    private MessageEntity message;
-//
-//    @Column(name="timestamp", nullable = false, unique = false, updatable = true)
-//    private LocalDateTime timestamp;
-//
-//    @Column(name="read", nullable = false, unique = false, updatable = true)
-//    private boolean read;
-//    public NotificationEntity() {
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public UserEntity getUser() {
-//        return user;
-//    }
-//
-//    public void setUser(UserEntity user) {
-//        this.user = user;
-//    }
-//
-//    public MessageEntity getMessage() {
-//        return message;
-//    }
-//
-//    public void setMessage(MessageEntity message) {
-//        this.message = message;
-//    }
-//
-//    public LocalDateTime getTimestamp() {
-//        return timestamp;
-//    }
-//
-//    public void setTimestamp(LocalDateTime timestamp) {
-//        this.timestamp = timestamp;
-//    }
-//
-//    public boolean isRead() {
-//        return read;
-//    }
-//
-//    public void setRead(boolean read) {
-//        this.read = read;
-//    }
-//}
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table (name = "notification")
+public class NotificationEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true, updatable = false)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false)
+    private UserEntity user;
+
+    @OneToOne
+    @JoinColumn(name="message")
+    private MessageEntity message;
+
+    @Column(name="timestamp", nullable = false, unique = false, updatable = true)
+    private LocalDateTime timestamp;
+
+    @Column(name="`read`", nullable = false, unique = false, updatable = true)
+    private boolean read;
+    public NotificationEntity() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public MessageEntity getMessage() {
+        return message;
+    }
+
+    public void setMessage(MessageEntity message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+}
